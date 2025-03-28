90076ac8ad7ab5ffcd6ace0bf867e20177bd05d0
Yara Anwer <yaraanwer099@gmail.com>
2025-03-28 14:43:25 +0200

    implemented random response generayion

---
+++ code2 .py
@@ -0,0 +1,13 @@
+import random
+
+respons=["yes,definitey!"
+        "No,not now.","Ask again later.",
+        "It is certain.",
+        "Very doubtful.",
+        "Outlook is good.",
+        "Better not tell you now.",
+        "Concenttrate and ask again."]
+
+def get_random_response():
+
+random.choice(respons)
